8178edd71c6abe7adb1baff1ba1097f511403c96
Dorothy Jane Ababon <dorothyjane.ababon@g.msuiit.edu.ph>
2023-03-20 16:04:39 +0800

    added login

--- dist/bundle.js
+++ dist/bundle.js
@@ -196,3 +196,26 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ })()
 ;
+
+const container = document.querySelector('.container');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
+
+registerLink.addEventListener('click', ()=> {
+  container.classList.add('active');
+})
+
+loginLink.addEventListener('click', ()=> {
+    container.classList.remove('active');
+  })
+
+
+btnPopup.addEventListener('click', ()=> {
+  container.classList.add('active-popup');
+})
+
+iconClose.addEventListener('click', ()=> {
+  container.classList.remove('active-popup');
+})
